00c9d09114e5b0ebf345290888b38e5cbe388cbe
taufiq33 <taufiqhidayat1908@gmail.com>
2022-08-20 19:14:59 +0700

    basic styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-indigo-700 text-2xl">Haloo</h1>
+      <button type="button" className="text-slate-100 tracking-widest font-bold h-10 p-2 rounded-lg bg-indigo-800 shadow-lg">Daftar Sekarang</button>
+    </>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './styles/tailwind.css';
+import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<p>Halo</p>);
+root.render(<App />);
