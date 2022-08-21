d14efe892770a8cc48f3f141917bf46d8b5ae06e
taufiq33 <taufiqhidayat1908@gmail.com>
2022-08-21 17:53:26 +0700

    install redux & kerangka awal ui app

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,18 @@ function Home() {
     <div>
       <LayoutSidebar
         sidebar={<SideNav items={menus} verticalAlign="top" />}
-        content={<div><h1>Haloo</h1></div>}
+        content={
+          (
+            <div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
+              <div className="w-full md:w-3/4 pl-5 pb-5">
+                Bagan utama aplikasi
+              </div>
+              <div className="w-full md:w-1/4 h-full shadow-lg bg-gray-100 border-r-2 border-white pl-5">
+                Bagian keranjang aplikasi
+              </div>
+            </div>
+          )
+        }
         sidebarSize={80}
       />
     </div>
