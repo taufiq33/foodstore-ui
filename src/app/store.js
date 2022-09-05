794580006ce6d6902d3515085e6bbe88e0e6bd41
taufiq33 <taufiqhidayat1908@gmail.com>
2022-09-05 07:06:04 +0700

    fix : typo using redux dev tools

--- src/app/store.js
+++ src/app/store.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 import authReducer from '../features/Auth/reducer';
 
 // eslint-disable-next-line no-underscore-dangle
-const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
   auth: authReducer,
