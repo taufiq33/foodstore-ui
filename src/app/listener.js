1580d7a6771f851b3adb2379c974e16054ca655d
taufiq33 <taufiqhidayat1908@gmail.com>
2022-10-21 09:07:47 +0700

    feat: sync cart on background

--- src/app/listener.js
+++ src/app/listener.js
@@ -1,12 +1,24 @@
 import store from './store';
+import { saveCart } from '../api/cart';
 
 let currentAuth;
+let currentCart;
+
 function listener() {
   const previousAuth = currentAuth;
   currentAuth = store.getState().auth;
 
+  const previousCart = currentCart;
+  currentCart = store.getState().cart;
+
   if (currentAuth !== previousAuth) {
     localStorage.setItem('auth', JSON.stringify(currentAuth));
+    saveCart(currentCart);
+  }
+
+  if (currentCart !== previousCart) {
+    localStorage.setItem('cart', JSON.stringify(currentCart));
+    saveCart(currentCart);
   }
 }
 
