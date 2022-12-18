998fbbf381a5cb982713625a6c5b3df5aade6c45
taufiq33 <taufiqhidayat1908@gmail.com>
2022-12-18 18:55:04 +0700

    feat: deliveryAddress hook&apiHelper

---
+++ src/api/address.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import config from '../config';
+import store from '../app/store';
+
+const { token } = store.getState().auth || JSON.parse(localStorage.getItem('auth'));
+
+async function getDeliveryAddress(params) {
+  // eslint-disable-next-line no-return-await
+  return await axios.get(`${config.apiHost}/deliveryaddress`, {
+    params: {
+      limit: params.limit,
+      skip: params.page * params.limit - params.limit,
+    },
+    headers: {
+      authorization: token,
+    },
+  });
+}
+
+async function createDeliveryAddress(payload) {
+  // eslint-disable-next-line no-return-await
+  return await axios.post(`${config.apiHost}/deliveryaddress`, payload, {
+    headers: {
+      authorization: token,
+    },
+  });
+}
+
+export {
+  createDeliveryAddress, getDeliveryAddress,
+};
---
+++ src/hooks/addressHook.js
@@ -0,0 +1,49 @@
+import { useCallback, useState, useEffect } from 'react';
+import { getDeliveryAddress } from '../api/address';
+
+const statusList = {
+  idle: 'idle',
+  process: 'process',
+  success: 'success',
+  error: 'error',
+};
+
+function useAddressHook() {
+  const [status, setStatus] = useState(statusList.idle);
+  const [addressData, setAddressData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(3);
+  const [addressDataCount, setAddressDataCount] = useState(0);
+
+  // eslint-disable-next-line prefer-arrow-callback, consistent-return
+  const fetchAddress = useCallback(async function getAddress() {
+    setStatus(statusList.process);
+    const { data: fetchResult } = await getDeliveryAddress(page, limit);
+    const { data, count, error } = fetchResult;
+
+    if (error) {
+      setStatus(statusList.error);
+      return false;
+    }
+
+    setStatus(statusList.success);
+    setAddressData(data);
+    setAddressDataCount(count);
+  }, [page, limit]);
+
+  useEffect(() => {
+    fetchAddress();
+  }, [fetchAddress]);
+
+  return {
+    status,
+    addressData,
+    page,
+    setPage,
+    limit,
+    setLimit,
+    addressDataCount,
+  };
+}
+
+export default useAddressHook;
