e08b75026c03de3843c925d68e230759858b1c41
taufiq33 <taufiqhidayat1908@gmail.com>
2023-01-02 20:54:02 +0700

    feat: implement protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import {
+  HashRouter, Routes, Route, Outlet, Navigate,
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { object, string } from 'prop-types';
 
 import store from './app/store';
 import listen from './app/listener';
@@ -15,7 +18,15 @@ import Login from './pages/Login';
 import UserAddressList from './pages/UserAddressList';
 import UserAddressAdd from './pages/UserAddressAdd';
 import Checkout from './pages/Checkout';
+import UserOrders from './pages/UserOrders';
 import Invoices from './pages/Invoices';
+
+function ProtectedRoute({ user, redirectPath = '/login' }) {
+  if (!user) {
+    return <Navigate to={redirectPath} replace />;
+  }
+  return <Outlet />;
+}
 
 function App() {
   React.useEffect(() => {
@@ -27,21 +38,34 @@ function App() {
       <Provider store={store}>
         <HashRouter>
           <Routes>
+            <Route element={<ProtectedRoute user={store.getState().auth.user} />}>
+              <Route path="/account" element={<UserAccount />} />
+              <Route path="/account/logout" element={<UserLogout />} />
+              <Route path="/alamat-pengiriman" element={<UserAddressList />} />
+              <Route path="/alamat-pengiriman/tambah" element={<UserAddressAdd />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/orders" element={<UserOrders />} />
+              <Route path="/invoice/:id" element={<Invoices />} />
+            </Route>
             <Route path="/" element={<Home />} />
-            <Route path="/account" element={<UserAccount />} />
-            <Route path="/account/logout" element={<UserLogout />} />
             <Route path="/register" element={<Register />} />
             <Route path="/register/berhasil" element={<RegisterSuccess />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/alamat-pengiriman" element={<UserAddressList />} />
-            <Route path="/alamat-pengiriman/tambah" element={<UserAddressAdd />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/invoice/:id" element={<Invoices />} />
           </Routes>
         </HashRouter>
       </Provider>
     </div>
   );
 }
+
+ProtectedRoute.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  user: object.isRequired,
+  redirectPath: string,
+};
+
+ProtectedRoute.defaultProps = {
+  redirectPath: '/login',
+};
 
 export default App;
